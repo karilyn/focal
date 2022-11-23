73d81c7c47825d8825d8925cbb8dae7670d77bd7
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-23 17:26:20 +0000

    pair programming challenge first commit

---
+++ objects/raisin.js
@@ -0,0 +1,9 @@
+// write a function searches an array for raisins
+
+const raisinAlarm = function (cookie) {
+
+};
+
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
